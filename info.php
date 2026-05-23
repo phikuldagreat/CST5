931229c95d5c0c2730a713f6b5af6b2d80553d37
Steven Deligero <stevendeligero11@gmail.com>
2026-05-23 19:05:16 +0800

    debugging

--- info.php
+++ info.php
@@ -1,2 +1,6 @@
 <?php
-phpinfo();
+echo PHP_VERSION;
+echo "<br>";
+echo extension_loaded('pdo_mysql') ? 'pdo_mysql loaded' : 'pdo_mysql NOT loaded';
+echo "<br>";
+echo extension_loaded('mysqli') ? 'mysqli loaded' : 'mysqli NOT loaded';
